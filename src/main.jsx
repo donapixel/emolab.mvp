2f5f8aa47402ae4d2ac2b01249fd6154bb89bc7e
donapixel <donadiomateo@gmail.com>
2026-08-01 03:08:02 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,3 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js").catch((err) => {
       console.error("[main] No se pudo registrar el Service Worker:", err);
-    });
-  });
-}
